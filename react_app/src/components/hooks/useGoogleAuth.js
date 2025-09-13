7648151b28b6af7adf0f4d8ac73caa61a7eeb396
hhyytt0126 <matsuurahayato0126@icloud.com>
2025-09-13 22:04:42 +0900

    update: Progress Modal

--- react_app/src/components/hooks/useGoogleAuth.js
+++ react_app/src/components/hooks/useGoogleAuth.js
@@ -1,15 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
-
+export let access_token = null;
+export function getAccessToken(){
+  return access_token;
+} 
 export function useGoogleAuth(CLIENT_ID, SCOPES) {
-  const [token, setToken] = useState(null);
   const tokenClientRef = useRef(null);
-
+  const [token, setToken] = useState(null);
   useEffect(() => {
     tokenClientRef.current = window.google.accounts.oauth2.initTokenClient({
       client_id: CLIENT_ID,
       scope: SCOPES,
       callback: (tokenResponse) => {
         setToken(tokenResponse.access_token);
+        access_token = tokenResponse.access_token;
       },
     });
   }, [CLIENT_ID, SCOPES]);
@@ -19,6 +22,7 @@ export function useGoogleAuth(CLIENT_ID, SCOPES) {
   };
 
   const logout = () => {
+    access_token = null;
     setToken(null);
   };
 
